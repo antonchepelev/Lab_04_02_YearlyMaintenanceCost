31cc8ff0337146d94479fdbf19171036afd38d40
antonchepelev <anton.chepelev@gmail.com>
2024-09-17 18:17:21 -0400

    completed task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //maintenance costs for every season
+
+        double summerCost = 305.76;
+
+        double fallCost = 474.14;
+
+        double winterCost = 634.58;
+
+        double springCost = 566.32;
+
+        double totalCost = summerCost + fallCost + winterCost + springCost;
+
+        System.out.println("These are the maintenance cost for the following months" +
+
+                System.lineSeparator()+  //newline regardless of which system user is using
+
+                "summer: "+summerCost+
+
+                System.lineSeparator()+
+
+                "fall: "+fallCost+
+
+                System.lineSeparator()+
+
+                "winter: "+winterCost+
+
+                System.lineSeparator()+
+
+                "spring: "+springCost+
+
+                System.lineSeparator()+
+
+                "The total yearly maintenance cost is: "+totalCost);
     }
 }
